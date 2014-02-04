ee859430fce3779da72c4cb8716aee2e66a9bdab
hewssteve <hewssteve@gmail.com>
2014-02-04 15:58:45 +1100

    Cleanups

--- GLEnginev2/src/Main.cpp
+++ GLEnginev2/src/Main.cpp
@@ -1,32 +1,9 @@
 #include "EngineMain.h"
 
 #include <iostream>
-#include <GL/glew.h>
-/*
-bool compileShader(Shader& shader, const std::string& source)
-{
-  std::cout << "==============================" << std::endl;
-  std::cout << "Shader source: " << std::endl;
-  std::cout << source << std::endl;
-  
-  shader.loadSource(source.c_str());
 
-  if (!shader.compile())
-  {
-    std::cout << "=======================================" << std::endl;
-    std::cout << "Shader compile error: (" << shader.getID() << ")"
-        << std::endl;
-    std::cout << shader.getCompileLog() << std::endl;
-    std::cout << "=======================================" << std::endl;
-    return false;
-  }
-  return true;
-}
-*/
 int main(int argc, char *argv[])
 {
-  std::cout << "Main()" << std::endl;
-
   WindowManager window;
 
   if(!window.init(800,600,false))
